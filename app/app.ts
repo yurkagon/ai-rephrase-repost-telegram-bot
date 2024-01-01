3e0801cc41a566eaa450a7fdc98effcc4ac77e76
Yurii Khvishchuk <owngameplay@gmail.com>
2024-01-02 01:02:00 +0200

    [ADD] Single image repost

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,38 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
+import { toHTML } from "@telegraf/entity";
+
+const targetChannel = "@test_yuragon";
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_API_TOKEN as string);
 bot.start((ctx) => ctx.reply("Welcome"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
-
 
 bot.on(message("text"), (ctx) => {
-  ctx.telegram.sendMessage("@test_yuragon", ctx.message.text)
+  console.log(ctx.message);
+});
+
+bot.on("channel_post", async (ctx: any) => {
+  if (ctx.channelPost.photo) {
+    const photo = ctx.channelPost.photo;
+    const caption = ctx.channelPost.caption;
+
+    const formattedCaption = caption + "\n<b>Author Yurii</b>";
+
+
+    await ctx.telegram.sendPhoto(
+      targetChannel,
+      photo[photo.length - 1].file_id,
+      {
+        caption: formattedCaption,
+        parse_mode: "HTML",
+      }
+    );
+  } else if (ctx.channelPost.text) {
+    const formattedText = toHTML(ctx.channelPost) + "\n<b>Author Yurii</b>";
+    await ctx.telegram.sendMessage(targetChannel, formattedText, {
+      parse_mode: "HTML",
+    });
+  }
 });
 
 bot.launch().then(() => console.info("Bot connected"));
