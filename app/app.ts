bfe5c2bc858b20cbaa4d300a5d779cf372167c75
Yurii Khvishchuk <owngameplay@gmail.com>
2024-01-02 17:58:18 +0200

    [ADD] Reposting multiimage posts

--- app/app.ts
+++ app/app.ts
@@ -12,17 +12,37 @@ bot.on(message("text"), (ctx) => {
   console.log(ctx.message);
 });
 
-const mediaGroupStore = {};
+const mediaGroupStore: Record<
+  string,
+  {
+    data: any[];
+    publish: () => Promise<void>;
+  }
+> = {};
 bot.on("channel_post", async (ctx: any) => {
   if (`@${ctx.channelPost.chat.username}` === targetChannel) return;
 
   if (ctx.channelPost.media_group_id) {
     const mediaGroupID = ctx.channelPost.media_group_id;
     if (!mediaGroupStore[mediaGroupID]) {
-      mediaGroupStore[mediaGroupID] = [];
+      mediaGroupStore[mediaGroupID] = {
+        data: [],
+        publish: _.debounce(async function () {
+          try {
+            console.log("FIRED", this.data);
+            await ctx.telegram.sendMediaGroup(targetChannel, this.data);
+          } catch (error) {
+            console.error("error", error);
+          } finally {
+            delete mediaGroupStore[mediaGroupID];
+          }
+        }, 1000),
+      };
     }
 
-    const highestQualityPhoto = _.last(ctx.channelPost.photo);
+    const highestQualityPhoto = _.last(
+      ctx.channelPost.photo as [{ file_id: string }]
+    );
     const mediaGroupItem = {
       type: "photo",
       media: highestQualityPhoto.file_id,
@@ -34,16 +54,11 @@ bot.on("channel_post", async (ctx: any) => {
       mediaGroupItem["parse_mode"] = "HTML";
     }
 
-    mediaGroupStore[mediaGroupID].push(mediaGroupItem);
+    mediaGroupStore[mediaGroupID].data.push(mediaGroupItem);
 
-    console.count(ctx.channelPost.media_group_id);
-    console.log("mediaGroupStore[mediaGroupID]", mediaGroupStore[mediaGroupID]);
-
-    // await ctx.telegram
-    //   .sendMediaGroup(targetChannel, mediaGroup)
-    //   .catch((e) => console.log(e));
+    await mediaGroupStore[mediaGroupID].publish();
   } else if (ctx.channelPost.photo) {
-    const photo = ctx.channelPost.photo;
+    const photo = ctx.channelPost.photo as [{ file_id: string }];
     const caption = ctx.channelPost.caption;
 
     const formattedCaption = formatHtmlText(caption);
@@ -60,11 +75,10 @@ bot.on("channel_post", async (ctx: any) => {
   }
 });
 
-
 const formatHtmlText = (text: string) => {
   if (!text) return "";
 
   return text + "\n<b>Author Yurii</b>";
-}
+};
 
 bot.launch();
